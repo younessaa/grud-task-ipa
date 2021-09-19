d398bddfb1a4e7365c86493c6e7bb92b7a13c5de
younessaa <y1o2u3n4@gmail.com>
2021-09-19 04:12:26 +0100

    implemented the review comment

--- database/mongoose.js
+++ database/mongoose.js
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://127.0.0.1:27017/taskmanagerdb')
     .then(() => {console.log("DB Conected Sucessfully!")})
     .catch(() => {
-        console.log(error)
+        console.log("Error occurred while DB connection ", error)
     });
 
 module.exports = mongoose;
